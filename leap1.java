e5e9ee3e83ae200f596792450b1bdf764e6dafc2
TPADMAPRIYA <37663101+TPADMAPRIYA@users.noreply.github.com>
2018-06-19 12:09:53 +0530

    Create leap1.java

---
+++ leap1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class leap1
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%4==0)
+{
+System.out.print("leap");
+}
+else
+{
+System.out.print("not");
+}
+}
+}
