931534623d50ec3ffd2cd0f4dbf0121cf3265d99
TPADMAPRIYA <37663101+TPADMAPRIYA@users.noreply.github.com>
2018-06-19 12:18:25 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class pos
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.print("positive");
+}
+else
+{
+System.out.print("negative");
+}
+}
+}
